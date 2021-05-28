2093a37f905f24cfb1999c42e2dc23a5ec74ad6f
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-05-28 16:19:44 -0500

    Agregado Style a Modal-Figure | script para dar Src y Alt a Modal-Figure__content y __caption

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,17 @@ const get = {
 const images = get.class("figure__image");
 
 for (let i = 0 ; i < images.length ; i++) {
-  images[i].addEventListener("click", (image) => {
-    console.log(image)
+  images[i].addEventListener("click", (info) => {
+    if (info.target.nextElementSibling.className == "modal") {
+      modal = info.target.nextElementSibling
+      modal.style.display = "block";
+
+      figure = modal.children[1];
+      figureImg = figure.children[0];
+      figureCaption = figure.children[1];
+
+      figureImg.src = info.target.src;
+      figureCaption.innerHTML = info.target.alt;
+    }
   })
 }
