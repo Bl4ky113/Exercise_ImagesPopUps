23c9377f25c5c8e90c443c9825b9aa799733d971
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-05-28 15:34:56 -0500

    Agregado Modal y EventListener a Imagenes

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 /* Script For Images PopUps */
+
+const get = {
+  id: document.getElementById.bind(document),
+  class: document.getElementsByClassName.bind(document),
+  tag: document.getElementsByTagName.bind(document)
+};
+
+const images = get.class("figure__image");
+
+for (let i = 0 ; i < images.length ; i++) {
+  images[i].addEventListener("click", (image) => {
+    console.log(image)
+  })
+}
